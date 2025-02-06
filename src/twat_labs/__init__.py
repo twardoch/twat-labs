3219a645c8508d1739dd8b94f8fc23ed05cd3381
twardoch <adam@twardoch.com>
2025-02-06 02:40:50 +0100

    Initial commit

---
+++ src/twat_labs/__init__.py
@@ -0,0 +1,5 @@
+"""twat labs plugin"""
+
+from importlib import metadata
+
+__version__ = metadata.version(__name__) 
---
+++ tests/test_package.py
@@ -0,0 +1,6 @@
+"""Test suite for twat_labs."""
+
+def test_version():
+    """Verify package exposes version."""
+    import twat_labs
+    assert twat_labs.__version__
